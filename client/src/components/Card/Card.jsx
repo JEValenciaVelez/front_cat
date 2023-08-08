d77385c8086ecf7fbcb57d65843fc5efb42678ec
unknown <juancho50301@gmail.com>
2023-08-08 13:14:17 -0500

    card-images

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -3,16 +3,17 @@ import "./Card.css";
 const Card = ({cats}) => {
 
     const allCats = cats.length > 0 ? cats : [];
-
+    let images = "https://cdn2.thecatapi.com/images/";
     return(
         <div className="Cards">
             {
                 allCats.map(cat => (
                     <div className="card" key={cat.id}>
                         <h2>{cat.name}</h2>
-                         <img src={cat.reference_image_id} alt={cat.name} />
+                         <img src={`${images}${cat.reference_image_id}.jpg`} alt={cat.name} />
                          <h2>{cat.temperament}</h2>
-                         <h4>{cat.life_span}</h4>
+                         <h2>intelligence : {cat.intelligence}</h2>
+                         <h4>Life: {cat.life_span}</h4>
                          <h2>{cat.origin}</h2>
                     </div>
                 ))
